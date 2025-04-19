9723b0e97d7dc1671ed5b683eec1a6f245b1ad6a
Rokki Khazratov <rokkikhazratov@gmail.com>
2025-04-20 00:24:30 +0500

    nadmozer

--- randomizer/views.py
+++ randomizer/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+import random
 
-# Create your views here.
 
 def home(request):
-    return render(request, 'randomizer/home.html')
+    result = None
+    min_num = None
+    max_num = None
+    
+    if request.method == 'POST':
+        try:
+            min_num = int(request.POST.get('min_number', 1))
+            max_num = int(request.POST.get('max_number', 100))
+            if min_num <= max_num:
+                result = random.randint(min_num, max_num)
+        except ValueError:
+            pass
+            
+    return render(request, 'home.html', {
+        'result': result,
+        'min_num': min_num,
+        'max_num': max_num
+    })
